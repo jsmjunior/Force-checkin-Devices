a565b22fc89d0fab23a3e7d3b1c7cf71e0daf178
José de Souza <jose441232@gmail.com>
2025-10-18 10:32:52 +0100

    credentials
    insert fields for credentials

--- forceCheckin.js
+++ forceCheckin.js
@@ -1,7 +1,7 @@
 const url = "https://eu1.mobileiron.com/";
 const forceCheckinEndpoint = "api/v1/devices/forceCheckin";
 let devices_info = [];
-
+let credentials = localStorage.getItem('apiCredentials') || '';
 function getDevices() {
     const input = document.getElementById('insert_devices').value;
     devices_info = input.split('\n').map(item => item.trim()).filter(item => item !== '');
@@ -35,16 +35,24 @@ async function executeForceCheckin() {
     callApiForDeviceForceCheckin(formData);
 
 }
+function saveCredentials() {
+    const credentials = btoa(`${document.getElementById('username').value}:${document.getElementById('password').value}`);
+    localStorage.setItem('apiCredentials', credentials);
+}
+function getCredentials() {
+    return localStorage.getItem('apiCredentials') || '';
+}
 
 function callApiForDeviceForceCheckin(formData) {
+
     fetch(`${url}${forceCheckinEndpoint}`, {
         method: 'PUT',
         headers: {
             'Content-Type': 'application/json',
             'Accept': 'application/json',
-            'Authorization': 'Basic ${credentials}' // Uncomment and set your API token if needed
+            'Authorization': 'Basic ' + getCredentials()
         },
-        body: "ids:3394905 ids:3789793",
+        body: JSON.stringify({ ids: devices_info })
     })
         .then(response => response.json())
         .then(data => {
